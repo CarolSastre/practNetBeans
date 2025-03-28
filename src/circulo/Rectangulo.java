f997a7a780a3657d748af0c1b0ed65619a3be5c9
csastre <carolsaga02@gmail.com>
2025-03-28 15:38:43 +0100

    Añadida clase rectangulo

---
+++ src/circulo/Rectangulo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package circulo;
+
+/**
+ *
+ * @author Carolina Sastre Garrido
+ * @grade 1º DAM Programacion
+ * @date 28 de març 2025
+ */
+public class Rectangulo {
+    float alto, ancho;
+    
+    public Rectangulo(float l1, float l2){
+        this.alto=l1;
+        this.ancho=l2;
+    }
+}
